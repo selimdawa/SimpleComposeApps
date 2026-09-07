ab111d1762c0b221026e5f8b02f113c12f356850
selimdawa <selimdawa@gmail.com>
2026-09-07 14:59:04 +0300

    Joke - Nav Add

--- app/src/main/java/com/flatcode/simplecomposeapps/joke/activity/JokeAppActivity.kt
+++ app/src/main/java/com/flatcode/simplecomposeapps/joke/activity/JokeAppActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.flatcode.simplecomposeapps.joke.ui.JokeScreen
 import com.flatcode.simplecomposeapps.joke.viewmodel.JokeViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,9 +23,17 @@ class JokeAppActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            JokeScreen(
-                viewModel = viewModel,
-            )
+            JokeNav(viewModel = viewModel)
+        }
+    }
+}
+
+@Composable
+fun JokeNav(viewModel: JokeViewModel) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") {
+            JokeScreen(viewModel = viewModel)
         }
     }
 }
